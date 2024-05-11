632cf5875ea3e72ddf8d5aadcc0a94e6a9763b8d
Sakya Rasadi <sakyarasadi@gmail.com>
2024-05-12 03:06:44 +0530

    feat: Add HistoryScreen to user_auth module

---
+++ flutterapp/flutter_firebase/lib/features/user_auth/presentation/pages/history.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HistoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('History')),
+      body: Center(
+        child: Text('History Screen'),
+      ),
+    );
+  }
+}
