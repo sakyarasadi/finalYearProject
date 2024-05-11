d277911e79e298a3180efff7ba5ef50c5674ec99
Sakya Rasadi <sakyarasadi@gmail.com>
2024-05-11 10:20:10 +0530

    feat: Add Firebase initialization to main.dart

---
+++ flutterapp/Medimate/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:flutter_firebase/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+         apiKey: "AIzaSyDv_FuPSfiA-quOIfsXYc_jgmx6KWpBdgg",
+          appId: "1:482092438756:web:43d354393529ae5f9088f1",
+          messagingSenderId: "482092438756",
+           projectId: "flutter-firebase-f5984",
+ 
+
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
