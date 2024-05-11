cfe202b57d9fb83497b2a8408da672d5c2414972
Sakya Rasadi <sakyarasadi@gmail.com>
2024-05-11 10:18:08 +0530

    feat: Add splash screen to Flutter app

---
+++ flutterapp/Medimate/lib/features/app/splash_screen/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final Widget? child;
+  const SplashScreen({Key? key, this.child}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => widget.child!),
+            (route) => false);
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('flutter_firebase/assests/images/stat.png'), // Add your image here
+            SizedBox(height: 20), // Adjust spacing as needed
+            Text(
+              "MediMate",
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20), // Adjust spacing as needed
+            CircularProgressIndicator(), // Loading icon
+          ],
+        ),
+      ),
+    );
+  }
+}
